1544d1039f39f4ceee5d2c72e5cc9dde62ccfc32
hallieking <hallie@wustl.edu>
2022-07-22 15:14:50 -0700

    small h animation

--- html/main.js
+++ html/main.js
@@ -32,3 +32,24 @@ function appear(){
         }
     }
 }
+
+
+document.addEventListener('DOMContentLoaded', function(e) {
+
+    var smallH = document.getElementById('small-h');
+    document.addEventListener('scroll', function(e) {
+        let documentHeight = document.body.scrollHeight;
+        let currentScroll = window.scrollY + window.innerHeight;
+        // When the user is [modifier]px from the bottom, fire the event.
+        let modifier = 200; 
+        if(currentScroll + modifier > documentHeight) {
+            console.log('You are at the bottom!')
+            smallH.classList.add('active');
+        }
+
+        else{
+            smallH.classList.remove('active');
+        }
+    })
+})
+
