1b5d90a029bd9512c48e9ab368e87fcd5c52ddc2
hallieking <hallie@wustl.edu>
2023-01-29 17:51:15 +0100

    website update

--- main.js
+++ main.js
@@ -34,22 +34,22 @@ function appear(){
 }
 
 
-document.addEventListener('DOMContentLoaded', function(e) {
+// document.addEventListener('DOMContentLoaded', function(e) {
 
-    var smallH = document.getElementById('small-h');
-    document.addEventListener('scroll', function(e) {
-        let documentHeight = document.body.scrollHeight;
-        let currentScroll = window.scrollY + window.innerHeight;
-        // When the user is [modifier]px from the bottom, fire the event.
-        let modifier = 200; 
-        if(currentScroll + modifier > documentHeight) {
-            console.log('You are at the bottom!')
-            smallH.classList.add('active');
-        }
+//     var smallH = document.getElementById('small-h');
+//     document.addEventListener('scroll', function(e) {
+//         let documentHeight = document.body.scrollHeight;
+//         let currentScroll = window.scrollY + window.innerHeight;
+//         // When the user is [modifier]px from the bottom, fire the event.
+//         let modifier = 200; 
+//         if(currentScroll + modifier > documentHeight) {
+//             console.log('You are at the bottom!')
+//             smallH.classList.add('active');
+//         }
 
-        else{
-            smallH.classList.remove('active');
-        }
-    })
-})
+//         else{
+//             smallH.classList.remove('active');
+//         }
+//     })
+// })
 
