0eea97584ae11c7326f52f280207b9cf2075a98a
hallieking <70177851+hallieking@users.noreply.github.com>
2024-10-06 22:58:07 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-// const exit = document.querySelector(".exit-btn");
-// const carousel = document.querySelector(".carousel");
-
-// let images = document.querySelectorAll(".image");
-
-// exit.addEventListener('click', () => {
-//     carousel.style.display = 'none';
-// });
-
-// for (let i = 0; i < images.length; i++){
-//     images[i].addEventListener('click', () =>{
-//         carousel.style.display = 'block';
-//     });
-// }
-
 window.addEventListener('scroll', appear);
 
 function appear(){
@@ -33,23 +18,9 @@ function appear(){
     }
 }
 
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden";
 
-// document.addEventListener('DOMContentLoaded', function(e) {
-
-//     var smallH = document.getElementById('small-h');
-//     document.addEventListener('scroll', function(e) {
-//         let documentHeight = document.body.scrollHeight;
-//         let currentScroll = window.scrollY + window.innerHeight;
-//         // When the user is [modifier]px from the bottom, fire the event.
-//         let modifier = 200; 
-//         if(currentScroll + modifier > documentHeight) {
-//             console.log('You are at the bottom!')
-//             smallH.classList.add('active');
-//         }
-
-//         else{
-//             smallH.classList.remove('active');
-//         }
-//     })
-// })
+});
 
